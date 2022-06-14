e11ada836d163e3620629aae659d374ddb7b6021
hoang.le <hoang.le@bstarsolutions.com>
2022-06-14 09:53:50 +0700

    add log link db

--- nodejs/data/connect.js
+++ nodejs/data/connect.js
@@ -1,19 +1,10 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(
-  // process.env.MONGODB_URI ||
-  // "mongodb://localhost:27017/appchatver5",
-  // "mongodb+srv://AstmCrim:Ok0901835614hh@cluster0.iw9xv.mongodb.net/cluster1?retryWrites=true&w=majority",
-  // "mongodb+srv://AstmCrim:Ok0901835614hh@cluster0.fopis.mongodb.net/cluster0?retryWrites=true&w=majority",
-  //   "mongodb+srv://AstmCrim:Ok0901835614hh@cluster0.fopis.mongodb.net/cluster0?retryWrites=true&w=majority",
-  //   "mongodb+srv://root:admin@cluster0.ebbq1.mongodb.net/test",
-  // `mongodb+srv://AstmCrim:admin@cluster0.iw9xv.mongodb.net/test`,
-  // `mongodb+srv://root:admin@cluster0.ebbq1.mongodb.net/firstdb`,
-  // `mongodb+srv://root:admin@cluster0.dovox.mongodb.net/firstdb?retryWrites=true&w=majority`,
-  // `mongodb+srv://${process.env.USERNAME_DB}:${process.env.PASSWORD_DB}@cluster0.dovox.mongodb.net/${process.env.DB}?retryWrites=true&w=majority`,
-  // `mongodb+srv://root:admin@cluster0.hpke1.mongodb.net/firstdb`,
-  `mongodb+srv://${process.env.USERNAME_DB}:${process.env.PASSWORD_DB}@cluster0.hpke1.mongodb.net/${process.env.DB}`,
+const DB_URL = `mongodb+srv://${process.env.USERNAME_DB}:${process.env.PASSWORD_DB}@cluster0.hpke1.mongodb.net/${process.env.DB}`;
+console.log("🚀 ~ file: connect.js ~ line 4 ~ DB_URL", DB_URL)
 
+mongoose.connect(
+  DB_URL,
   { useNewUrlParser: true, useUnifiedTopology: true },
   () => console.log("Connect mongoose success")
 );
