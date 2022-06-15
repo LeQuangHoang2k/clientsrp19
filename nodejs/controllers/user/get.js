e4f574ace972fb889c1307e50314bb9725c56d70
hoang.le <hoang.le@bstarsolutions.com>
2022-06-15 13:23:53 +0700

    setup multi request

--- nodejs/controllers/user/get.js
+++ nodejs/controllers/user/get.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { UsersModel } = require("../../models/Users/users");
+const axios = require("axios");
 // get
 
 exports.getAll = async (req, res) => {
@@ -10,6 +11,30 @@ exports.getAll = async (req, res) => {
   console.log("🚀 ~ file: get.js ~ line 10 ~ getAll ~ users", users);
 
   // main
+
+  const promise1 = Promise.resolve(3);
+  const promise2 = 42;
+  const promise3 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 100, "foo");
+  });
+
+  const a = axios.get("http://10.0.100.131:8080/api/v1/products/all");
+  const b = axios.get("http://10.0.100.131:8080/api/v1/products/categories");
+  const c = axios.get("http://10.0.100.131:8080/api/v1/products/suppliers");
+
+  // Promise.all([b, c]).then((values) => {
+  //   values.forEach((element) => {
+  //     console.log(element.data.createdAt);
+  //   });
+  // });
+
+  const mutilResponses = await Promise.all([b, c]);
+  mutilResponses.forEach((element) => {
+    console.log(
+      "🚀 ~ file: get.js ~ line 26 ~ exports.getAll= ~ res",
+      element.data.createdAt
+    );
+  });
 
   //res
 
--- nodejs/models/Users/users.js
+++ nodejs/models/Users/users.js
@@ -9,3 +9,39 @@ const UsersSchema = new mongoose.Schema({
 
 exports.UsersModel = mongoose.model("users", UsersSchema);
 
+// id int [pk, increment] // auto-increment
+// email varchar
+// password varchar
+
+
+// first_name varchar
+// last_name varchar
+// phone varchar
+// image varchar
+
+// city varchar
+// district varchar
+// ward varchar
+// apartment_number varchar
+
+// permission_id number
+// rank_id number
+// state_id number
+
+// created_at timestamp
+// updated_at timestamp
+
+// "email": "a1@a.com",
+//     "password":"123456789Aa@",
+//     "first_name":"hoang",
+//     "last_name":"le",
+//     "phone":"0123456789",
+//     "image":"https://",
+//     "city":"",
+//     "district":"",
+//     "ward":"",
+//     "apartment_number":"123 abc",
+//     "permission_id":2,
+//     "rank_id":5,
+//     "state_id":1
+
