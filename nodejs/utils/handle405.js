cc0228c4b46be053f76b31ba92dbf7456406a34b
hoang.le <hoang.le@bstarsolutions.com>
2022-06-20 15:40:43 +0700

    refactor code

--- nodejs/utils/handle405.js
+++ nodejs/utils/handle405.js
@@ -1,5 +1,8 @@
 exports.handle405 = (req, res) => {
+  //   console.log(req.method);
+  //   console.log("🚀 ~ file: handle405.js ~ line 5 ~ res.status ~ req.ip", req.ip);
   res.status(405).json({
     message: "Method not allow",
+    alert: `${req.ip} tried to access ${req.originalUrl}`,
   });
 };
