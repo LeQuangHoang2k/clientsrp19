08a04d6c8f56725feac54b4381a7671fb79b0e8f
bstardev\hoang.le <hoang.le@bstarsolutions.com>
2022-06-24 12:36:46 +0700

    test config

--- nodejs/controllers/cart/add-to-cart/add-to-cart.controller.js
+++ nodejs/controllers/cart/add-to-cart/add-to-cart.controller.js
@@ -42,6 +42,7 @@ exports.addToCart = async (req, res) => {
 //       {
 //         _id: "62b014e2a4fd7db9b18c0d57",
 //         name: null,
+
 //         // name: `iphone 12 pro max #1`,
 //         price: 20000000,
 //         image: "http://abc.com",
